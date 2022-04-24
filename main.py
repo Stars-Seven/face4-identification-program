8cc7e5aaca3cb2c45a459db45e3654d8130d0c2d
unknown <toshi780724@gmail.com>
2022-04-24 12:45:39 +0900

    first

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model, Sequential
+from keras.models import Model
+from tensorflow.keras.preprocessing import image
+import tensorflow.compat.v1 as tf
+
+import numpy as np
+
+
+classes = ['Cute', 'Feminine', 'Cool', 'Elegant']
+image_size = 64
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./model_joyu4_random9057_npz.h5')#学習済みモデルをロード
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            #受け取った画像を読み込み、np形式に変換
+            img = image.load_img(filepath, target_size=(image_size,image_size))
+            img = image.img_to_array(img)
+            data = np.array([img])
+            #変換したデータをモデルに渡して予測する
+            result = model.predict(data)[0]
+            predicted = result.argmax()
+            pred_answer = "あなたは " + classes[predicted] + " タイプです"
+
+            return render_template("index.html",answer=pred_answer, images=filepath)
+
+    return render_template("index.html",answer="")
+
+
+if __name__ == "__main__":
+    app.run()
+    #port = int(os.environ.get('PORT', 8080))
+    #app.run(host ='0.0.0.0',port = port)
